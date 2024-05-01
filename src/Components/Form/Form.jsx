45705db47e09b4a276a5743070ca646fe0e27605
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2024-05-01 18:40:21 +0200

    Delete functional

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -4,7 +4,7 @@ import styles from './Form.module.css';
 function Form() {
 	
 	const [expenseList, setExpenseList] = useState([]);
-	const [expense, setExpense] = useState({title:'', amount:'', date:'', category:''});
+	const [expense, setExpense] = useState({title:'', amount:'', date:'', category:'-'});
 	const [errorMsg, setErrorMsg] = useState({});
 	const [isInputValid, setisInputValid] = useState(false);
 
@@ -48,11 +48,19 @@ function Form() {
 		if (isValid) {
 			console.log('Submission successfully completed');
 			setExpenseList(prev=>[...prev, expense]);
-			setExpense({title:'', amount:'', date:'', category:''})
+			setExpense({title:'', amount:'', date:'', category:'-'})
 		} else {
 			console.log('Submission failed. Error');
 		}
 	}
+
+	const handleDelete =(indexToDelete)=>{
+		const newExpenseArray = expenseList.filter((_, index)=> {
+			return index !== indexToDelete;
+		});
+		setExpenseList(newExpenseArray);
+	}
+
 
   return (
 	<div className={styles.formContainer}>
@@ -73,7 +81,7 @@ function Form() {
 			<div>
 				<label htmlFor="category">Category</label>
 				<select onChange={handleChange} name="category" id="category" value={expense.category}>
-					<option value="">--</option>
+					<option value="&ndash;">--</option>
 					<option value="housing">Housing</option>
 					<option value="groceries">Groceries</option>
 					<option value="transportation">Transportation</option>
@@ -90,6 +98,7 @@ function Form() {
 						<span>{expense.amount}</span>
 						<span>{expense.date}</span>
 						<span>{expense.category}</span>
+						<button className={styles.deleteButton} onClick={()=>handleDelete(index)}>Remove</button>
 					</div>
 				})}
 		</section>
